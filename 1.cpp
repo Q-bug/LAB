05f0aa109902db1c86d1378df24b2faa5186b3bf
Qanonymous <104785817+Q-bug@users.noreply.github.com>
2022-09-26 16:45:31 +0300

    Create 1.cpp
    nothing to do

---
+++ 1.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main()
+{
+    std::cout<<"Input versts: ";
+    long double inp;
+    std::cin>>inp;
+    std::cout<<"Sazhens:     "<<inp*500<<std::endl;
+    std::cout<<"Сentimeters: "<<inp*500*213.36<<std::endl;
+
+    return 0;
+}
